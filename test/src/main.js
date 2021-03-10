2e48b95d568568ee66b5d9d22032d2ebe26fe0b7
damou_ <mouadsehbaoui@gmail.com>
2021-03-10 22:46:02 +0100

    main.js import new dependencies; router, bulma , darkmode

--- test/src/main.js
+++ test/src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import './../node_modules/bulma/css/bulma.css'
+import Darkmode from 'darkmode-js';
+
+new Darkmode().showWidget();
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+    router,
+    render: h => h(App),
 }).$mount('#app')
