575e36f8abbe501dc6f664b986c35fd7092dc040
damou_ <mouadsehbaoui@gmail.com>
2021-03-11 16:22:07 +0100

    refactor: Pokemon.vue
    added gif and changed port number for API calls.

--- test/src/components/Pokemon.vue
+++ test/src/components/Pokemon.vue
@@ -6,13 +6,14 @@
       </button>
     </div>
     <div class="columns mt-5">
-      <span class="column is-2 is-offset-3">which pokemon are you looking for ? : {{ benaddi }}</span>
-      <label>
-        <input v-model="benaddi">
+      <span class="column is-2 is-offset-2">which pokemon are you looking for ? : {{ benaddi }}</span>
+      <label class="column is-3">
+        <input v-model="benaddi" placeholder="Moi J'sui kEEEEEvin">
       </label>
       <button v-on:click="getPokemon"
-              class="column is-1 ml-5 button is-inverted is-rounded is-warning is-light">getOnePokemon
+              class="column is-1 button is-inverted is-rounded is-warning is-light">getOnePokemon
       </button>
+      <img class="column is-1 is-offset-1" src="https://media.giphy.com/media/YPz8UN8BIr5urfhxdT/giphy.gif">
     </div>
     <div class="columns mt-5">
       <h1 class="column is-full title is-3">All the Pokemons</h1>
@@ -33,10 +34,10 @@ export default {
   name: "Pokemon",
   methods: {
     getPokemons() {
-      axios.get("http://localhost:3000/api/pokemon").then(response => this.$data.pokemons.push(response.data))
+      axios.get("http://localhost:3001/api/pokemon").then(response => this.$data.pokemons.push(response.data))
     },
     getPokemon() {
-      axios.get("http://localhost:3000/api/pokemon/" + this.benaddi).then(response => this.$data.pokemons.push(response.data))
+      axios.get("http://localhost:3001/api/pokemon/" + this.benaddi).then(response => this.$data.pokemons.push(response.data))
     },
   },
   data() {
